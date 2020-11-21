b60ea96017eb880c4c3e352f5fe4052b956913fb
Jan Hovland <jan.hovland@lyse.net>
2020-11-21 09:25:50 +0100

    Initial Commit

---
+++ Shared/hoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  hoverApp.swift
+//  Shared
+//
+//  Created by Jan Hovland on 21/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct hoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
