6cea099bddd8d07243be4dbd0078dc77811deaa2
Jan Hovland <jan.hovland@lyse.net>
2020-11-22 12:58:17 +0100

    Update

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -1,21 +1,35 @@
-//
-//  ContentView.swift
-//  Shared
-//
-//  Created by Jan Hovland on 21/11/2020.
-//
+////
+////  ContentView.swift
+////  Shared
+////
+////  Created by Jan Hovland on 21/11/2020.
+////
 
 import SwiftUI
+import Foundation
 
 struct ContentView: View {
+    
+    @State private var overImage = false
+    @State private var text = ""
+    
+    /// Virker best om musen flyttes oppover
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack {
+                Text(text)
+                Image(systemName: "gear")
+                    .onHover { over in
+                        self.overImage = over
+                        if over {
+                            text = "Hjelpe tekst for knapp A"
+                        } else {
+                            text = ""
+                        }
+                    }
+            }
+            .navigationTitle("Hover test application")
+        }
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+    
 }
